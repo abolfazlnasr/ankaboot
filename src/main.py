bf4dcd4cec8fa82680fcafbae19ba3779517abb6
abolfazlnasr <49614051+abolfazlnasr@users.noreply.github.com>
2021-12-11 01:45:03 +0330

    feat: extrcat kewwords using yake and keybert

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from crawler import crawl
 from fastapi import FastAPI
 from starlette.responses import FileResponse
 from word_cloud import create_wordcloud_from_text
+from keyword_extractor import keywords_using_keybert, keywords_using_yake
 
 extracted_words = []
 default_url = "https://fa.wikipedia.org/wiki/%D8%A8%D8%B1%D9%86%D8%A7%D9%85%D9%87%E2%80%8C%D9%86" \
@@ -27,3 +28,15 @@ def show_word_cloud():
     text = " ".join(extracted_words)
     create_wordcloud_from_text(text)
     return FileResponse("img/result.png")
+
+
+@app.get("/keybert")
+def show_keywords_using_keybert():
+    text = " ".join(extracted_words)
+    return keywords_using_keybert(text)
+
+
+@app.get("/yake")
+def show_keywords_using_yake():
+    text = " ".join(extracted_words)
+    return keywords_using_yake(text)
